50f940e3101e8dbba836b894795cb1e09084b22c
Ludovico Marino <113300950+F1nnLM@users.noreply.github.com>
2023-09-26 12:56:34 +0200

    Update Mary.py
    added 8ball command

--- Mary.py
+++ Mary.py
@@ -1,7 +1,9 @@
 import discord
 from discord.ext import commands
+import random
 
 client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
 
 @client.event
 async def on_ready():
@@ -11,5 +13,11 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send("pong!")
 
+@client.command(aliases=["pallaotto", "8ball", "magicball"])
+async def eightball(ctx, *, question):
+    with open("response.txt", "r") as f:
+        random_resp = f.readlines()
+        response = random.choice(random_resp)
+    await ctx.send(response)
 
 client.run("MTE1NTc2NjkyMTE0MTE3MDE5Nw.GnUj_c.gdiLnWll8oLZpo4yTHF7UQJG19vsqV1DmslY0s")
